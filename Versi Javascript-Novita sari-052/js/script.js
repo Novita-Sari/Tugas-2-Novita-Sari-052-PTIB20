68a8ac2a6155261d0e8b065f73bf0bf2f3060767
Novita-Sari <82359850+Novita-Sari@users.noreply.github.com>
2022-03-14 13:58:53 +0700

    Initial commit

---
+++ Versi Javascript-Novita sari-052/js/script.js
@@ -0,0 +1,15 @@
+/*  Memunculkan Pop Up Button  */
+alert("SELAMAT DATANG DI WEBSITE INI");
+
+/*  Memanipulasi Teks  */
+document.getElementById("prodi1").innerHTML =
+  "Program studi Teknik Informatika adalah prodi yang akan mempelajari berbagai prinsip terkait ilmu komputer mulai dari proses perancangan, pengembangan, pengujian, hingga evaluasi sistem operasi perangkat lunak. Selama kuliah kamu akan banyak mengkaji pemrograman dan komputasi, dan dibekali pula dengan keterampilan merancang perangkat lunak.";
+
+document.getElementById("prodi2").innerHTML =
+  "Program studi Pendidikan Teknologi Informasi merupakan program studi yang bertujuan untuk mempersiapkan seorang inovator pendidikan yang tidak hanya berkompeten dalam merancang suatu inovasi pembelajaran, namun juga terampil dalam memanfaatkan perkembangan teknologi informasi untuk tercapainya tujuan pembelajaran atau kompetensi yang diharapkan dari peserta didik.";
+
+document.getElementById("prodi3").innerHTML =
+  "Program studi Sistem Informasi adalah bidang keilmuan yang menggabungkan ilmu komputer dengan bisnis dan manajemen. Di prodi ini kamu akan belajar gimana mengidentifikasi kebutuhan dan proses bisnis perusahaan berdasarkan data-data yang dimiliki perusahan, kemudian merancang sistem yang sesuai dengan kebutuhan perusahaan.";
+
+document.getElementById("prodi4").innerHTML =
+  "Program studi Manajemen Informatika adalah sebuah program studi di perguruan tinggi yang mengajarkan mahasiswanya tentang penggunaan teknologi untuk memudahkan kegiatan bisnis. Mahasiswa Jurusan Manajemen Informatika belajar tentang cara menganalisis, merancang, mengembangkan, hingga mengelola teknologi informasi.";
